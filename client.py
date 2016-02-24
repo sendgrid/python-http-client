ca61ba314e68c69cccfb265853e346efb0f643e9
Elmer Thomas <elmer@thinkingserious.com>
2016-02-24 12:10:14 -0800

    Clarifications

--- client.py
+++ client.py
@@ -15,14 +15,14 @@ class Client(object):
     def __init__(self, host=None, api_key=None, headers=None):
         self.host = host
         self.request_headers = {'Authorization': 'Bearer ' + api_key, 'Content-Type': 'application/json'}
-        self.methods = ['get', 'post', 'put', 'patch', 'delete']
+        self.methods = ['delete', 'get', 'patch', 'post', 'put']
         if headers:
             self._set_headers(headers)
         self._count = 0
         self._cache = {}
         self._status_code = None
         self._body = None
-        self._headers = None
+        self._response_headers = None
         self._response = None
 
     def _reset(self):
@@ -34,15 +34,7 @@ class Client(object):
         self._cache[self._count] = value
         self._count += 1
 
-    def _build_url(self):
-        url = ""
-        count = 0
-        while count < len(self._cache):
-            url += "/" + self._cache[count]
-            count += 1
-        return self.host + url
-
-    def _build_urllib_url(self, params):
+    def _build_url(self, params):
         url = ""
         count = 0
         while count < len(self._cache):
@@ -54,14 +46,9 @@ class Client(object):
         return self.host + url
 
     def _set_response(self, response):
-        self._status_code = response.status_code
-        self._body = response.text
-        self._headers = response.headers
-
-    def _set_urllib_response(self, response):
         self._status_code = response.getcode()
         self._body = response.read()
-        self._headers = response.info()
+        self._response_headers = response.info()
 
     def _set_headers(self, headers):
         self.request_headers.update(headers)
@@ -72,19 +59,19 @@ class Client(object):
 
     def __getattr__(self, value):
         if value in self.methods:
-            method = value.upper() 
+            method = value.upper()
             def http_request(*args, **kwargs):
                 if 'headers' in kwargs:
                     self._set_headers(kwargs['headers'])
                 data = json.dumps(kwargs['data']) if 'data' in kwargs else None
                 params = kwargs['params'] if 'params' in kwargs else None
                 opener = urllib.build_opener()
-                request = urllib.Request(self._build_urllib_url(params), data=data)  
+                request = urllib.Request(self._build_url(params), data=data)  
                 for key, value in self.request_headers.iteritems():
                     request.add_header(key, value)
                 request.get_method = lambda: method
                 self._response = opener.open(request)
-                self._set_urllib_response(self._response)
+                self._set_response(self._response)
                 self._reset()
                 return self
             return http_request
@@ -102,4 +89,4 @@ class Client(object):
 
     @property
     def headers(self):
-        return self._headers
+        return self._response_headers
