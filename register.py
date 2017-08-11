6a3a63e2511b3df9c9ef23eebd5bcd80ad8821ae
Elmer Thomas <elmer@thinkingserious.com>
2017-08-11 13:48:07 -0700

    Version Bump v5.0.0: fixes #328 and #321

--- register.py
+++ register.py
@@ -1,10 +1,14 @@
-import pandoc
+import pypandoc
 import os
 
-pandoc.core.PANDOC_PATH = '/usr/local/bin/pandoc'
-
-doc = pandoc.Document()
-doc.markdown = open('README.md').read()
+output = pypandoc.convert('README.md', 'rst')
 f = open('README.txt','w+')
-f.write(doc.rst)
+f.write(output)
 f.close()
+
+readme_rst = open('./README.txt').read()
+replace = '[SendGrid Logo]\n(https://uiux.s3.amazonaws.com/2016-logos/email-logo%402x.png)'
+replacement = '|SendGrid Logo|\n\n.. |SendGrid Logo| image:: https://uiux.s3.amazonaws.com/2016-logos/email-logo%402x.png\n   :target: https://www.sendgrid.com'
+final_text = readme_rst.replace(replace,replacement)
+with open('./README.txt', 'w') as f:
+  f.write(final_text)
