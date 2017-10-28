c27edf034d6f401e7d62cde011900dbf509bb1af
Siddu <siddaram.halli@edgeverve.com>
2017-10-28 12:21:29 +0530

    Removed empty lines

--- examples/live_sendgrid_example.py
+++ examples/live_sendgrid_example.py
@@ -8,9 +8,8 @@ request_headers = {
     "Authorization": 'Bearer {0}'.format(api_key)
 }
 version = 3  # we could also use client.version(3)
-client = python_http_client.Client(host=host,
-                                   request_headers=request_headers,
-                                   version=version)
+client = python_http_client.Client(host=host, 
+    request_headers=request_headers, version=version)
 
 # GET collection
 response = client.api_keys.get()
