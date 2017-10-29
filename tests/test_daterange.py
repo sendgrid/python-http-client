c7b7e62cb2585f6109d70b27564617b0be4c8c33
dinosaurfiles <dinosaurfiles@users.noreply.github.com>
2017-10-29 10:33:56 +0800

    Update code for PEP8 compliance

--- tests/test_daterange.py
+++ tests/test_daterange.py
@@ -6,10 +6,14 @@ try:
 except ImportError:
     import unittest
 
+
 class DateRangeTest(unittest.TestCase):
     def setUp(self):
-        self.openlicensefile = os.path.join(os.path.dirname(__file__), '../LICENSE.txt')
-        self.pattern = 'Copyright (c) 2012 - %s SendGrid, Inc.' % (time.strftime("%Y"))
+        self.openlicensefile = os.path.join(
+                                os.path.dirname(__file__),
+                                '../LICENSE.txt')
+        self.pattern = 'Copyright (c) 2012 - %s SendGrid, Inc.' % (
+                        time.strftime("%Y"))
         self.licensefile = open(self.openlicensefile).read()
 
     def test__daterange(self):
