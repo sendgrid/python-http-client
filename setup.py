8af820ff1ad0f6199ec6775adbd5dc5405cadb6e
Hugo <hugovk@users.noreply.github.com>
2018-10-04 08:06:31 +0300

    Remove Python 2 classifier

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'API'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
