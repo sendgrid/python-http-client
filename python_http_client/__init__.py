96b528bcb09af8e561ae4e4772235c5338f67d6c
Demian Brecht <demianbrecht@gmail.com>
2019-11-05 15:46:15 -0800

    fix unclosed version file warning

--- python_http_client/__init__.py
+++ python_http_client/__init__.py
@@ -19,4 +19,5 @@ from .exceptions import (  # noqa
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if os.path.isfile(os.path.join(dir_path, 'VERSION.txt')):
-    __version__ = open(os.path.join(dir_path, 'VERSION.txt')).read().strip()
+    with open(os.path.join(dir_path, 'VERSION.txt')) as fh:
+        __version__ = fh.read().strip()
